98cc57f4ee054132730f4387a105d1d10e397db5
Pralay Umale <104301489+Pralay6@users.noreply.github.com>
2023-09-20 23:13:12 +0530

    Add files via upload

---
+++ src/Board.js
@@ -0,0 +1,8 @@
+import React from "react";
+import App from "./App";
+
+function Board(props) {
+  return <div className="board" {...props} />;
+}
+
+export default Board;
---
+++ src/Square.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Square(props) {
+  return (
+    <div className={"square"} {...props}>
+      {props.x ? "x" : props.o ? "o" : ""}
+    </div>
+  );
+}
+export default Square;
